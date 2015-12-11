1a823f94c47723a40005fd47f4894b0dcbce6a6d
Manuel <manubp9@gmail.com>
2015-12-11 13:28:04 +0100

    empezado metodo showStats que muestra las estadisticas de todos los mensajes

--- MailClient.java
+++ MailClient.java
@@ -9,33 +9,45 @@
 
 public class MailClient
 {
-	//El servidor al que esta conectado este cliente
-	private MailServer server;
-	//La direccion del usuario que esta usando este cliente
-	private String user;
-	//El ultimo email recibido
-	private MailItem lastMail;
-	//contador de spam
-	private int spam;
+    //El servidor al que esta conectado este cliente
+    private MailServer server;
+    //La direccion del usuario que esta usando este cliente
+    private String user;
+    //El ultimo email recibido
+    private MailItem lastMail;
+    //contador de spam
+    private int spam;
+    //mensajes envi ados
+    private int enviados;
+    //mensajes recibidos
+    private int recibidos;
+    //porcentaje de spam
+    private float porcentajeSpam;
+     
+    //direccion de la persona que nos envio el mensaje mas largo junto con cuantos caracteres tenia el mensaje
 
-	/**
-	 * Crea un objeto MailClient a partir de los valores dados
-	 */
-	public MailClient(MailServer server, String user)
-	{
-		this.user = user;
-		this.server = server;
-		lastMail = null;
-	}
+    /**
+     * Crea un objeto MailClient a partir de los valores dados
+     */
+    public MailClient(MailServer server, String user)
+    {
+        this.user = user;
+        this.server = server;
+        lastMail = null;
+        enviados = 0;
+        recibidos = 0;
+        spam = 0;
+        porcentajeSpam = 0;
+    }
 
-	/**
-	 * Recupera del servidor el siguiente email destinado
-	 * al usuario que esta usando el cliente. Si no hay 
-	 * ningun email pendiente de ser descargado devuelve null; si
-	 * lo hay, devuelve el email
-	 */
-	public MailItem getNextMailItem()
-	{
+    /**
+     * Recupera del servidor el siguiente email destinado
+     * al usuario que esta usando el cliente. Si no hay 
+     * ningun email pendiente de ser descargado devuelve null; si
+     * lo hay, devuelve el email
+     */
+    public MailItem getNextMailItem()
+    {
         MailItem mail = server.getNextMailItem(user);
         if (mail != null) {
             String mensaje = mail.getMessage();
@@ -51,15 +63,15 @@ public class MailClient
         }
         return mail;
     }
-	/**
-	 * Recupera del servidor el siguiente email destinado al
-	 * usuario que esta usando el cliente e imprime sus datos
-	 * por pantalla. Si no hay ningun email imprime por pantalla
-	 * un mensaje advirtiendo de ello
-	 */
-	public void printNextMailItem()
-	{
-	    MailItem item = server.getNextMailItem(user);
+    /**
+     * Recupera del servidor el siguiente email destinado al
+     * usuario que esta usando el cliente e imprime sus datos
+     * por pantalla. Si no hay ningun email imprime por pantalla
+     * un mensaje advirtiendo de ello
+     */
+    public void printNextMailItem()
+    {
+        MailItem item = server.getNextMailItem(user);
         String mensaje = item.getMessage();
         if (mensaje.contains("trabajo"))
         {
@@ -81,58 +93,69 @@ public class MailClient
 
     }
 
-	/**
-	 * Permite redactar un email indicando el destinatario y el
-	 * cuerpo del mensaje y enviarlo al servidor
-	 */
-	public void sendMailItem(String to, String message, String subject)
-	{
-		MailItem email = new MailItem(user, to, message, subject);
-		server.post(email);
-	}
-	
-		
-	/**
-	 * Muestra por pantalla el numero de mensajes pendientes de
-	 * descargar en el servidor para el usuario actual
-	 */
-	public void howManyMailItemsIHave()
-	{
-		System.out.println("Emails pendientes en el servidor: " + 
-		                   server.howManyMailItems(user));
-	}
-	
-	/**
-	 * Recibe un correo y responde automaticamente indicando
-	 * que estamos fuera de la oficina
-	 */
-	public void getNextMailItemAndSendAutomaticRespond()
-	{	
-		MailItem email = getNextMailItem();
-		if (email != null)
-		{
-			sendMailItem(email.getFrom(),
-			             "No estoy en la oficina. \n" + email.getMessage(),
-			             "RE: " + email.getSubject());
-		}
-	}
-	
-	/**
-	 * Muestra por pantalla los datos del ultimo email recibido.
-	 * En caso de no haber recibido aun ningun email, informa de ello.
-	 */
-	public void muestraUltimoEmail() 
-	{
-		if(lastMail != null) {
-			lastMail.print();
-		}
-		else {
-			System.out.println("No hay ningún mensaje.");
-		}
-	}
-	/**
-	 * Muestra por pantalla los datos del ultimo email recibido.
-	 * En caso de no haber recibido aun ningun email, informa de ello.
-	 */
-	public void () 
-}
+    /**
+     * Permite redactar un email indicando el destinatario y el
+     * cuerpo del mensaje y enviarlo al servidor
+     */
+    public void sendMailItem(String to, String message, String subject)
+    {
+        MailItem email = new MailItem(user, to, message, subject);
+        server.post(email);
+        enviados = enviados + 1;
+    }
+    
+        
+    /**
+     * Muestra por pantalla el numero de mensajes pendientes de
+     * descargar en el servidor para el usuario actual
+     */
+    public void howManyMailItemsIHave()
+    {
+        System.out.println("Emails pendientes en el servidor: " + server.howManyMailItems(user));
+        
+    }
+    
+    /**
+     * Recibe un correo y responde automaticamente indicando
+     * que estamos fuera de la oficina
+     */
+    public void getNextMailItemAndSendAutomaticRespond()
+    {   
+        MailItem email = getNextMailItem();
+        if (email != null)
+        {
+            sendMailItem(email.getFrom(),
+                         "No estoy en la oficina. \n" + email.getMessage(),
+                         "RE: " + email.getSubject());
+        }
+    }
+    
+    /**
+     * Muestra por pantalla los datos del ultimo email recibido.
+     * En caso de no haber recibido aun ningun email, informa de ello.
+     */
+    public void muestraUltimoEmail() 
+    {
+        if(lastMail != null) {
+            lastMail.print();
+        }
+        else {
+            System.out.println("No hay ningún mensaje.");
+        }
+    }
+    /**
+     *El cliente de correo electrónico a través de un método llamado showStats sea capaz de mostrar 
+     *por pantalla unas estadísticas que incluyan el número de mensajes enviados, el número de mensajes recibidos,
+     *el número de mensajes que son spam, el porcentaje de spam 
+     *y la dirección de la persona que nos envío el mensaje más largo junto con cuantos caracteres tenía ese mensaje.
+     * 
+     */
+    public void showStats() 
+    {
+        System.out.println( "numero de mensajes enviados: " , enviados);
+        System.out.println
+        System.out.println
+        System.out.println
+        
+    }
+} 
