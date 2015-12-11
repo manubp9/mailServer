cbe94520d7e21cba99565bef211591812c35cafe
Manuel <manubp9@gmail.com>
2015-12-11 13:05:47 +0100

    modificado metodos print nextmailitem y getnexmailitem para detectar spam

--- MailClient.java
+++ MailClient.java
@@ -6,61 +6,133 @@
  * @version (a version number or a date)
  */
 
+
 public class MailClient
 {
-    // instance variables - replace the example below with your own
-    private MailServer server;
-    private String user;
+	//El servidor al que esta conectado este cliente
+	private MailServer server;
+	//La direccion del usuario que esta usando este cliente
+	private String user;
+	//El ultimo email recibido
+	private MailItem lastMail;
+	//contador de spam
+	private int spam;
 
-    /**
-     * Constructor for objects of class MailClient
-     */
-    public MailClient(MailServer server ,String usuario )
-    {
-        // initialise instance variables
-        this.server = server ;
-        this.user = usuario ;
+	/**
+	 * Crea un objeto MailClient a partir de los valores dados
+	 */
+	public MailClient(MailServer server, String user)
+	{
+		this.user = user;
+		this.server = server;
+		lastMail = null;
+	}
+
+	/**
+	 * Recupera del servidor el siguiente email destinado
+	 * al usuario que esta usando el cliente. Si no hay 
+	 * ningun email pendiente de ser descargado devuelve null; si
+	 * lo hay, devuelve el email
+	 */
+	public MailItem getNextMailItem()
+	{
+        MailItem mail = server.getNextMailItem(user);
+        if (mail != null) {
+            String mensaje = mail.getMessage();
+            if (mensaje.contains("trabajo")) {
+            }
+            else if (mensaje.contains("regalo") || mensaje.contains("promocion")) {
+                spam = spam + 1;
+                mail = null;
+            }
+            if (mail != null) {
+                lastMail = mail;
+            }
+        }
+        return mail;
+    }
+	/**
+	 * Recupera del servidor el siguiente email destinado al
+	 * usuario que esta usando el cliente e imprime sus datos
+	 * por pantalla. Si no hay ningun email imprime por pantalla
+	 * un mensaje advirtiendo de ello
+	 */
+	public void printNextMailItem()
+	{
+	    MailItem item = server.getNextMailItem(user);
+        String mensaje = item.getMessage();
+        if (mensaje.contains("trabajo"))
+        {
+            item.print();
+        }
+        else{
+            if(mensaje.contains("regalo") || (mensaje.contains("promocion")))
+            {
+                System.out.println ("El mensaje recibido es un spam");
+                spam = spam + 1;
+
+            }
+            else 
+            {
+                item.print();
+            }
+
+        }
 
     }
 
-    /**
-     * metodo que devuelve el ultimo mensaje de un usuario
-     */
-    public MailItem  getNextMailItem()
-    {
-        return server.getNextMailItem( user );
-
-    }
-
-    /**
-     * imprimir por pantalla si el usuario tiene mensajes
-     */
-    public void printNextMailItem()
-    {
-        MailItem email = getNextMailItem();
+	/**
+	 * Permite redactar un email indicando el destinatario y el
+	 * cuerpo del mensaje y enviarlo al servidor
+	 */
+	public void sendMailItem(String to, String message, String subject)
+	{
+		MailItem email = new MailItem(user, to, message, subject);
+		server.post(email);
+	}
+	
+		
+	/**
+	 * Muestra por pantalla el numero de mensajes pendientes de
+	 * descargar en el servidor para el usuario actual
+	 */
+	public void howManyMailItemsIHave()
+	{
+		System.out.println("Emails pendientes en el servidor: " + 
+		                   server.howManyMailItems(user));
+	}
+	
+	/**
+	 * Recibe un correo y responde automaticamente indicando
+	 * que estamos fuera de la oficina
+	 */
+	public void getNextMailItemAndSendAutomaticRespond()
+	{	
+		MailItem email = getNextMailItem();
 		if (email != null)
 		{
-			//Imprimimos los detalles del email
-			email.printMail();
-			
-			System.out.println("De: " + email.getFrom());
-			System.out.println("Destinatario: " + email.getTo());
-			System.out.println("Mensaje" + email.getMessage());
+			sendMailItem(email.getFrom(),
+			             "No estoy en la oficina. \n" + email.getMessage(),
+			             "RE: " + email.getSubject());
 		}
-		else 
-		{
-			//Avisamos de que no hay emails en el servidor
-			System.out.println("No hay correo nuevo");	
-        }
-    }
-
-    /**
-     * envia el mensaje al receptor
-     */
-    public void sendMailItem(String to , String message, String subject)
-    {
-        MailItem email = new MailItem(user,to,message, subject);
-        server.post(email);
-    }
-
+	}
+	
+	/**
+	 * Muestra por pantalla los datos del ultimo email recibido.
+	 * En caso de no haber recibido aun ningun email, informa de ello.
+	 */
+	public void muestraUltimoEmail() 
+	{
+		if(lastMail != null) {
+			lastMail.print();
+		}
+		else {
+			System.out.println("No hay ningún mensaje.");
+		}
+	}
+	/**
+	 * Muestra por pantalla los datos del ultimo email recibido.
+	 * En caso de no haber recibido aun ningun email, informa de ello.
+	 */
+	public void () 
 }
--- MailItem.java
+++ MailItem.java
@@ -4,60 +4,69 @@
  */
 public class MailItem
 {
-    // quien envia el mensaje
-    private String from;
-    // receptor del mensaje
-    private String to;
-    // mensaje
-    private String message;
-    // crea un asunto
-    private String subject;
-    
-    /**
-     * Constructor que inicia las variables de MailItem
-     */
-    public MailItem(String from, String to, String message, String subject)
-    {
-        // initialise instance variables
-        this.from = from;
-        this.to = to;
-        this.message = message;
-        this.subject = subject;
-    }
+	//La direccion del remitente
+	private String from;
+	//La dirección del receptor
+	private String to;
+	//El cuerpo del mensaje de email
+	private String message;
+	//El asunto del email
+	private String subject;
 
-    /**
-     * Método que devuelve el atributo from
-     */
-    public String getFrom()
-    {
-        return from;
-    }
+	/**
+	 * Crea un objeto MailItem inicializando sus atributos
+	 * a los valores dados como parametros
+	 */
+	public MailItem(String from, String to, String message, String subject)
+	{
+		this.from = from;
+		this.to = to;
+		this.message = message;
+		this.subject = subject;
+	}
+	
+	/**
+	 * Devuelve la direccion el remitente
+	 */
+	public String getFrom(){
+		return from;
 
-    /**
-     * Método que devuelve el atributo for
-     */
-    public String getTo()
-    {
-        return to;
-    }
+	}
+	
+	/**
+	 * Devuelve la dirección del receptor
+	 */
+	public String getTo(){
+		return to;
+	}
+	
+	/**
+	 * Devuelve el cuerpo del mensaje
+	 */
+	public String getMessage() {	
 
-    /**
-     * Método que devuelve la cadena almacenada en from
-     */
-    public String getMessage()
-    {
-        return message;
-    }
+		return message;
+	}
+	
+	/**
+	 * Devuelve el asunto del mensaje
+	 */
+	public String getSubject()
+	{
+		return subject;
+	}
 
-    /**
-     * Imprime por pantalla el nombre del emisor el receptor y el mensaje
-     */
-    public void printMail()
-    {
-        System.out.println("from: " + from);
-        System.out.println("to: " + to);
-        System.out.println("message: " + message);
-        System.out.println("subject: " + subject);
-    }
+	/**
+	 * Imprime por pantalla el remitente, el receptor y el
+	 * cuerpo del email
+	 */
+	public void print()
+	{
+		System.out.println("De: " + from);
+		System.out.println("Para: " + to);
+		System.out.println("Asunto: " + subject);
+		System.out.println("Mensaje: " + message);
+	}
+
+	
 }
-
